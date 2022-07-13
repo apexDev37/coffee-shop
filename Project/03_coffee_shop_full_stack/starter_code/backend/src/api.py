dcf343f974774cd54475f909f39f4f90b98d3d73
apexdev <mwangi.em37@gmail.com>
2022-07-13 10:52:16 +0300

    Initialized SQLite DB

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -17,7 +17,7 @@ CORS(app)
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 # ROUTES
 '''
@@ -25,8 +25,9 @@ CORS(app)
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
+        returns status code 200 and json {"success": True, "drinks": drinks}
+        where drinks is the list of drinks or appropriate
+        status code indicating reason for failure
 '''
 
 
@@ -35,8 +36,9 @@ CORS(app)
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
+        returns status code 200 and json {"success": True, "drinks": drinks}
+        where drinks is the list of drinks or appropriate
+        status code indicating reason for failure
 '''
 
 
@@ -46,8 +48,9 @@ CORS(app)
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
+        returns status code 200 and json {"success": True, "drinks": drinks}
+        where drinks is the list of drinks or appropriate
+        status code indicating reason for failure
 '''
 
 
@@ -59,8 +62,9 @@ CORS(app)
         it should update the corresponding row for <id>
         it should require the 'patch:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
+        returns status code 200 and json {"success": True, "drinks": drinks}
+        where drinks is the list of drinks or appropriate
+        status code indicating reason for failure
 '''
 
 
@@ -71,8 +75,9 @@ CORS(app)
         it should respond with a 404 error if <id> is not found
         it should delete the corresponding row for <id>
         it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
+        returns status code 200 and json {"success": True, "drinks": drinks}
+        where drinks is the list of drinks or appropriate
+        status code indicating reason for failure
 '''
 
 
