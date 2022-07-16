c297cbd08531f94aa99ab510d695c5999c14b7d1
apexdev <mwangi.em37@gmail.com>
2022-07-16 13:16:26 +0300

    Auth0 credentials update
    Commented out secret variable credentials preceding <git push> command

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -2,6 +2,10 @@
  * ensure all variables on this page match your project
  */
 
+// Load environment variables
+// Enter Auth0 developer credentials in auth0 object
+
+
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
@@ -9,6 +13,6 @@ export const environment = {
     url: '', // the auth0 domain prefix
     audience: '', // the audience set for the auth0 app
     clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'https://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
